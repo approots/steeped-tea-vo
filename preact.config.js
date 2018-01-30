5383d3553e387b7ee1bebe5c664a68eaf164e634
rick <rick@coalescentdesign.com>
2018-01-29 19:32:12 -0800

    For Netlify redirect all routes to index.html for SPA

---
+++ preact.config.js
@@ -0,0 +1,7 @@
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+export default config => {
+  config.plugins.push(
+    new CopyWebpackPlugin([{ context: `${__dirname}/src`, from: `_redirects` }])
+  );
+};
